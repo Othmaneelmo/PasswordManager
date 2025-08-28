1c96a7eef6741b6ac703f79c1a13acf727b65dff
Othmaneelmo <oelmo031@uottawa.ca>
2025-08-27 23:01:43 -0400

    start to refactor main (hashing the mastery key in a separate file)

--- Main.java
+++ Main.java
@@ -52,107 +52,103 @@
  */
 
 
-import java.io.Console;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Arrays;
-import java.util.Base64;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
+  import java.io.Console;
+  import java.io.IOException;
+  import java.security.NoSuchAlgorithmException;
+  import java.security.SecureRandom;
+  import java.security.spec.InvalidKeySpecException;
+  import java.util.Arrays;
+  import java.util.Base64;
+  import javax.crypto.SecretKeyFactory;
+  import javax.crypto.spec.PBEKeySpec;
 
-public class Main {
-  public static void main(String[] args) throws IOException {
-/*
-    using char array then converting to string because:
-        - String are immutable, they are saved in memory
-        - can be hacked (memory dump) before java garbage collector removes it
-    Using console and not scanner because:
-        - Scanner shows password when typed
-*/
-    Console console = System.console();
-    if (console == null) {
-      System.out.println("Console unavailable. Please run in a terminal environment.");
-      return;
-    }
-
-    //Check if vaultStorage exists
-//    if (VaultStorage.exists()) {
-//      return;
-//    }
-    char[] masterKeyChars = console.readPassword("Create a Master key: ");
-
-
-//PASSWORD VALIDATION:
-/* 
- * This prints out whether the inputted password was valid or not, with a list of reason why not:
- *  - it takes masterKeyChars (char[]) as a parameter, 
- *  - PasswordValidator.validate() method checks for password complexity, and add reason X if rule Y is not respected
- *  - PasswordValidator.validate() method returns ValidationResult(reasons.isEmpty(), reasons)
- *  - the latter ValidationResult() returns whether the passowrd is valid (boolean ok), and the reasons (List<String> messages)
- *  - from the above return statements, this code section below prints appropriate messages(according to the return stats), AND CLEARS THE INVALID PASSWORD (char[])
-*/
-    ValidationResult vr = PasswordValidator.validate(masterKeyChars);
-    if (!vr.ok()) {
-      System.out.println("Password not strong enough:");
-      for (String msg : vr.messages()){
-        System.out.println(" - " + msg);
+  public class Main {
+    public static void main(String[] args) throws IOException {
+  /*
+      using char array then converting to string because:
+          - String are immutable, they are saved in memory
+          - can be hacked (memory dump) before java garbage collector removes it
+      Using console and not scanner because:
+          - Scanner shows password when typed
+  */
+      Console console = System.console();
+      if (console == null) {
+        System.out.println("Console unavailable. Please run in a terminal environment.");
+        return;
       }
-      Arrays.fill(masterKeyChars, ' '); // clear array before exiting
-      return;
-    }
 
-    // --- PBKDF2 placeholders ---
-    SecureRandom saltGenerator = new SecureRandom();
-    byte[] salt = new byte[16]; // 16 bytes = 128 bits
-    saltGenerator.nextBytes(salt);
-    // Encode salt in Base64 for storage
-    String encodedSalt = Base64.getEncoder().encodeToString(salt);
-    int iterations = 600_000;   // high iteration count
-    int keyLength = 256;        // key length in bit
-      
-    try {
-        // Create PBEKeySpec with char[] password, salt, iterations, key length
-        PBEKeySpec spec = new PBEKeySpec(masterKeyChars, salt, iterations, keyLength);
-
-        // Generate the PBKDF2 hash
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        byte[] hash = skf.generateSecret(spec).getEncoded();
-
-        // Clear the password inside PBEKeySpec
-        spec.clearPassword();
-        // Encode hash in Base64 for storage
-        String encodedHash = Base64.getEncoder().encodeToString(hash);
+      char[] masterKeyChars = console.readPassword("Create a Master key: ");
 
 
-        // Store as algorithm:iterations:salt:hash
-        String stored = "PBKDF2WithHmacSHA256" + ":" 
-                      + iterations + ":" 
-                      + encodedSalt + ":" 
-                      + encodedHash;
-        //remove this when done
-        System.out.println("PBKDF2 hash generated and stored:");
-        System.out.println(stored);
-      
-        /*TODO: store hash + encodedsalt + iterations securely*/
-        System.out.println("PBKDF2 hash generated successfully!");
-
-        if (VaultStorage.exists()) {
-            VaultStorage.saveMasterKey("PBKDF2WithHmacSHA256", iterations, encodedSalt, encodedHash);
+  //PASSWORD VALIDATION:
+  /* 
+  * This prints out whether the inputted password was valid or not, with a list of reason why not:
+  *  - it takes masterKeyChars (char[]) as a parameter, 
+  *  - PasswordValidator.validate() method checks for password complexity, and add reason X if rule Y is not respected
+  *  - PasswordValidator.validate() method returns ValidationResult(reasons.isEmpty(), reasons)
+  *  - the latter ValidationResult() returns whether the passowrd is valid (boolean ok), and the reasons (List<String> messages)
+  *  - from the above return statements, this code section below prints appropriate messages(according to the return stats), AND CLEARS THE INVALID PASSWORD (char[])
+  */
+      ValidationResult vr = PasswordValidator.validate(masterKeyChars);
+      if (!vr.ok()) {
+        System.out.println("Password not strong enough:");
+        for (String msg : vr.messages()){
+          System.out.println(" - " + msg);
         }
+        Arrays.fill(masterKeyChars, ' '); // clear array before exiting
+        return;
+      }
 
+      // --- PBKDF2 placeholders ---
+      SecureRandom saltGenerator = new SecureRandom();
+      byte[] salt = new byte[16]; // 16 bytes = 128 bits
+      saltGenerator.nextBytes(salt);
+      // Encode salt in Base64 for storage
+      String encodedSalt = Base64.getEncoder().encodeToString(salt);
+      int iterations = 600_000;   // high iteration count
+      int keyLength = 256;        // key length in bit
         
-    } catch (NoSuchAlgorithmException e) {
-    System.out.println("PBKDF2 algorithm not available: " + e.getMessage());
-    } catch (InvalidKeySpecException e) {
-    System.out.println("Invalid PBKDF2 key specification: " + e.getMessage());
+      try {
+          // Create PBEKeySpec with char[] password, salt, iterations, key length
+          PBEKeySpec spec = new PBEKeySpec(masterKeyChars, salt, iterations, keyLength);
 
-    }finally {
-    Arrays.fill(masterKeyChars, ' '); // ensures cleanup in all cases
+          // Generate the PBKDF2 hash
+          SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+          byte[] hash = skf.generateSecret(spec).getEncoded();
+
+          // Clear the password inside PBEKeySpec
+          spec.clearPassword();
+          // Encode hash in Base64 for storage
+          String encodedHash = Base64.getEncoder().encodeToString(hash);
+
+
+          // Store as algorithm:iterations:salt:hash
+          String stored = "PBKDF2WithHmacSHA256" + ":" 
+                        + iterations + ":" 
+                        + encodedSalt + ":" 
+                        + encodedHash;
+          //remove this when done
+          System.out.println("PBKDF2 hash generated and stored:");
+          System.out.println(stored);
+        
+          /*TODO: store hash + encodedsalt + iterations securely*/
+          System.out.println("PBKDF2 hash generated successfully!");
+
+          if (VaultStorage.exists()) {
+              VaultStorage.saveMasterKey("PBKDF2WithHmacSHA256", iterations, encodedSalt, encodedHash);
+          }
+
+          
+      } catch (NoSuchAlgorithmException e) {
+      System.out.println("PBKDF2 algorithm not available: " + e.getMessage());
+      } catch (InvalidKeySpecException e) {
+      System.out.println("Invalid PBKDF2 key specification: " + e.getMessage());
+
+      }finally {
+      Arrays.fill(masterKeyChars, ' '); // ensures cleanup in all cases
+      }
+
+
+
     }
-
-
-
   }
-}
