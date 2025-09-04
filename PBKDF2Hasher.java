e1f42ad6ecdbd6779c4afd4508e09f81480c976f
Othmaneelmo <oelmo031@uottawa.ca>
2025-09-04 17:54:50 -0400

    refactor PBKDF2Hasher, add verifyPassword method

--- PBKDF2Hasher.java
+++ PBKDF2Hasher.java
@@ -13,18 +13,45 @@ public class PBKDF2Hasher {
     // Generate a random salt (16 bytes)
     public static byte[] generateSalt() {
         SecureRandom saltGenerator = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[SALT_LENGTH];
         saltGenerator.nextBytes(salt);
         return salt;
     }
 
-    // Hash a password with PBKDF2
+    // Hash a password with PBKDF2 (using provided salt and iterations)
     public static String hashPassword(char[] password, byte[] salt, int iterations)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
+    throws NoSuchAlgorithmException, InvalidKeySpecException 
+    {
+
         PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, KEY_LENGTH);
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
         byte[] hash = skf.generateSecret(spec).getEncoded();
         spec.clearPassword(); // wipe sensitive data
         return Base64.getEncoder().encodeToString(hash);
+    }
+
+    //Hash using default constants
+    public static HashedPassword defaultHashPassword(char[] password)
+    throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        byte[] salt = generateSalt();
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String encodedHash = hashPassword(password, salt, DEFAULT_ITERATIONS);
+        return new HashedPassword("PBKDF2WithHmacSHA256", DEFAULT_ITERATIONS, encodedSalt, encodedHash);
+    }
+
+    //verify Password
+    public static boolean verifyPassword(char[] password, HashedPassword stored)
+    throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        byte[] salt = Base64.getDecoder().decode(stored.getSalt());
+        String testHash = hashPassword(password, salt, stored.getIterations());
+        return testHash.equals(stored.getHash());
+        /*
+        Only using ".equals" :
+        No need to use "constant time equals" to compare  inputted hash with stored hash,
+        timing attacks are unrealistic with PBKDF2 hashes
+        */
+
     }
 }
