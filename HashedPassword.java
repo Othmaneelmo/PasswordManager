ef7ab93d2465984ec9945ac033bb70bff5d92771
Othmaneelmo <oelmo031@uottawa.ca>
2025-09-04 17:05:45 -0400

    create HashedPassword file to help refactor PBKDF2Hasher

---
+++ HashedPassword.java
@@ -0,0 +1,29 @@
+public class HashedPassword {
+    private final String algorithm;
+    private final int iterations;
+    private final String salt;
+    private final String hash;
+
+    public HashedPassword(String algorithm, int iterations, String salt, String hash){
+        this.algorithm = algorithm;
+        this.iterations = iterations;
+        this.salt = salt;
+        this.hash = hash;
+    }
+
+    public String getAlgorithm(){
+        return algorithm;
+    }
+    public int getIterations(){
+        return iterations;
+    }
+    public String getSalt() {
+        return salt;
+    }
+    public String getHash() {
+        return hash;
+    }
+
+
+
+}
