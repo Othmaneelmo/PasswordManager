64ae652c76e089648ad0f4d1b01d5c8da5e6836b
Othmaneelmo <oelmo031@uottawa.ca>
2026-01-07 16:31:30 -0500

    Initialize VaultSecurityTest skeleton

---
+++ com/passwordmanager/test/VaultSecurityTest.java
@@ -0,0 +1,15 @@
+package com.passwordmanager.test;
+
+/**
+ * Advanced test suite for the password manager vault core.
+ *
+ * This class will host security, correctness, concurrency,
+ * and resilience tests for the vault subsystem.
+ */
+public class VaultSecurityTest {
+
+    public static void main(String[] args) {
+        // Test execution entry point
+    }
+
+}
