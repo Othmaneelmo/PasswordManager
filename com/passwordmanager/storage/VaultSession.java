71e9fc40f2c623b9189df01db622c9b31ed671b6
Othmaneelmo <oelmo031@uottawa.ca>
2026-01-07 10:21:45 -0500

    remove outdated comments from VaultSession class for clarity

--- com/passwordmanager/storage/VaultSession.java
+++ com/passwordmanager/storage/VaultSession.java
@@ -1,12 +1,4 @@
 package com.passwordmanager.storage;
-/*The following class:
- * 
- * VaultSession manages the unlocked state of the vault.
- *
- * - Locked by default.
- * - When unlocked, it holds the derived AES key in memory.
- * - When locked, the key is securely wiped and no operations are possible.
-*/
 
 import java.util.Arrays;
 import javax.crypto.SecretKey;
