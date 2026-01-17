7867d2ef49e48842e524fa256f61534d26886500
Othmaneelmo <oelmo031@uottawa.ca>
2026-01-17 01:50:03 -0500

    add EncryptedFileFormat class file with description

---
+++ com/passwordmanager/crypto/EncryptedFileFormat.java
@@ -0,0 +1,36 @@
+package com.passwordmanager.crypto;
+/**
+ * Defines the encrypted file format specification.
+ * <p>
+ * <b>File Structure:</b>
+ * </p>
+ * <pre>
+ * [Magic Header: 8 bytes] "VAULTENC"
+ * [Format Version: 1 byte] 0x01
+ * [Security Profile: 1 byte] 0x00=FAST, 0x01=BALANCED, 0x02=PARANOID
+ * [IV Length: 2 bytes] big-endian uint16
+ * [IV: variable] initialization vector
+ * [Ciphertext + Tag: variable] encrypted data with embedded auth tag
+ * </pre>
+ * 
+ * <p><b>Security Properties:</b></p>
+ * <ul>
+ *   <li>Self-describing - all metadata included for decryption</li>
+ *   <li>Version-tagged - supports future format changes</li>
+ *   <li>Tamper-evident - authenticated encryption detects modifications</li>
+ *   <li>Algorithm-agnostic - profile byte maps to crypto parameters</li>
+ * </ul>
+ * 
+ * <p><b>Future Extensibility:</b></p>
+ * <ul>
+ *   <li>Version byte allows format evolution</li>
+ *   <li>Can add compression flag, key rotation metadata</li>
+ *   <li>Can embed steganographic markers</li>
+ *   <li>Can add file integrity manifests</li>
+ * </ul>
+ */
+public class EncryptedFileFormat {
+
+    
+    
+}
